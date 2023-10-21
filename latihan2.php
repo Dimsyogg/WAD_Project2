cd373d241efb74b7e6b6e91e729ec9573de56ae1
Dimsyogg <106477675+Dimsyogg@users.noreply.github.com>
2023-10-21 11:30:49 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
+
+    <body>
+    <div class="container">
+    <div class="box">
+    
+
+    <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <input type="text" name="angka" pattern="[0-9]" placeholder="Input Angka">
+    <br><br><br>
+    <input type="number">
+    <br><br>
+    <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        pilihan
+    </button>
+    <ul class="dropdown-menu">
+        <li><a>pilihan 1</a></li>
+        <li><a>pilihan 2</a></li>
+        <li><a>pilihan 3</a></li>
+    </ul>
+    </div>
+    <br><br><br>
+    <button type="button" class="btn btn-primary">hitung</button>
+    </div>
+    </div>
+    </body>
+
+</html>
